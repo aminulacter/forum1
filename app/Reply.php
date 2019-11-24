4262735478952afa8515af9308db956737c3f892
Aminul Acter <acterprime1@gmail.com>
2019-11-24 08:45:35 +0600

    beggining

---
+++ app/Reply.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    public function thread()
+    {
+        return $this->belongsTo('App\Thread');
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo('App\User','user_id');
+    }
+}
